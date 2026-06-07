59c4b67401a219a035b0339b0eb0351f71e38f4e
prathameshchaskar <prathameshchaskar1@gmail.com>
2026-06-07 13:40:25 +0530

    Initial auth-service skeleton

---
+++ auth-service/src/test/java/com/shedlr/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shedlr.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
